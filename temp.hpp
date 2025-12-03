c0e737bc138c1afe142bfe6d1ff282762318a3c2
Federico Pizzolato <pizzolatofefe@gmail.com>
2025-12-03 15:50:20 +0100

    Initialized the local git repo, added some comments and the write_output function

--- temp.hpp
+++ temp.hpp
@@ -2,7 +2,8 @@
 // merge collision and propagation: stream_collide_save (page 565)
 // (((each cell pulls from its neighbors (no push) to avoid race conditions (page 647))))
 // ghost/halo cells => extra rows/columns
-// (((pipeline idea: send boundary data with MPI -> compute inner cells -> receive boundary data with MPI -> compute boundary cells)))
+// (((pipeline idea: compute tile-boundary cells, send boundary data with MPI to adjacent tiles
+//         -> compute inner cells while receiving boundary data with MPI -> compute boundary cells)))
 
 // to store (Lattice):
 // - f_current and f_next (ping-pong to avoid race conditions while computing collision and propagation)
@@ -32,6 +33,7 @@ struct LatticeDescriptor {
 
 };
 
+
 template <typename T>
 concept isDescriptor = requires{
 
@@ -39,6 +41,7 @@ concept isDescriptor = requires{
     T::q;
 
 } && std::derived_from<T, LatticeDescriptor<T::d, T::q>>;
+
 
 struct D2Q9 : public LatticeDescriptor<2, 9> {
 
@@ -56,6 +59,7 @@ struct D2Q9 : public LatticeDescriptor<2, 9> {
 
 };
 
+
 template <isDescriptor Descriptor>
 class Lattice {
 
@@ -63,21 +67,23 @@ public:
 
     Lattice(int nx, int ny) : u(Descriptor.d), f_current(Descriptor.q), f_next(Descriptor.q), u_w(Descriptor.d * 2, std::vector<double>(Descriptor.d)); // constructor
 
-    std::vector<std::vector<double>> f_current;
-    std::vector<std::vector<double>> f_next;
+    std::array<std::vector<double>, Descriptor::q> f_current;
+    std::array<std::vector<double>, Descriptor::q> f_next;
+    std::array<std::vector<double>, Descriptor::d> u;
     std::vector<double> rho;
-    std::vector<std::vector<double>> u;
     std::vector<double> sigma;
-    std::vector<std::vector<std::vector<double>>> u_w;
+    std::array<std::array<std::vector<double>, Descriptor::d>, 2> u_w;
 
+    // Do we need this if we have a single function for the solve?
     void swap_buffers();
 
     // functions for boundaries
     void boundary_values(const std::vector<int> &coords, double &rho_b, std::vector<double> &u_w);
 
-    // functions for indices
+    // from matrix indeces return vector indicex
     inline int idx(const std::vector<int> &coords);
 };
+
 
 template <isDescriptor Descriptor>
 class Solver {
@@ -87,19 +93,23 @@ private:
     double tau;
     Lattice<Descriptor> &lattice;
 
-    inline double feq(int i, double rho, double ux, double uy); // helper function to compute equilibrium
+    // helper function to compute equilibrium
+    inline double feq(int i, double rho, double ux, double uy); 
 
 public:
 
-    Solver(double kinematic_viscosity, Lattice<Descriptor> &lattice) : lattice(lattice); // constructor
+    // constructor
+    Solver(double kinematic_viscosity, Lattice<Descriptor> &lattice) : lattice(lattice); 
 
     void compute_moments(Lattice<Descriptor>& grid);
     void stream_collide_inner(Lattice<Descriptor>& grid);
-    // (((between these there will be MPI communication)))
+    // (((between these there could be MPI communication)))
     void stream_collide_boundaries(Lattice<Descriptor>& grid);
     void apply_bcs(Lattice<Descriptor>& grid, double u_lid);
 
-    // function for the output
+    // function for the output, maybe change second parameter, 
+    // use flag to set what output you want
+    void write_output(Lattice<Descriptor>& grid, char* out_buffer, char flag);
 
 };
 
