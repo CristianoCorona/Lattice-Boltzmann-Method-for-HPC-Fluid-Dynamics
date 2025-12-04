ae7d09be9b1fbf2d15f7a65bacad8ce5ae798ab4
Simone Ferri <simone1.ferri@mail.polimi.it>
2025-12-04 18:46:20 +0100

    Solver refinements and comments

--- include/solver.hpp
+++ include/solver.hpp
@@ -1,38 +1,37 @@
+#include "lattice.hpp"
+#include <array>
+
+#ifndef SOLVER_HPP
+#define SOLVER_HPP
+
 // Solver for LBM simulation
 // Handles:
 // - Stream and collide operations (merged to avoid race conditions)
 // - Boundary conditions
 // - Moment computation
-// - Output writing
-//
-// Notes:
-// - feq (equilibrium) computed as register variable
-// - fi* (collision and propagation are merged)
-// - Each cell pulls from its neighbors (no push) to avoid race conditions
-
-// assumption: Lattice has a function to which we pass coordinates, direction, rho_w, u_w and it gives us a boolean (to indicate if it's a 
-// boundary or not) and, if true, also rho_w, u_w of the wall/lid through parameters
-// so boundary_values is a bool: bool boundary_values(const std::vector<int> &coords, int i, float_type &rho_b, float_type &u_b);
-
-#include "lattice.hpp"
-
-#ifndef SOLVER_HPP
-#define SOLVER_HPP
-
 template <isDescriptor Descriptor, std::floating_point float_type = double>
 class Solver {
 
 private:
 
-    Lattice<Descriptor, float_type> &lattice;
+    /*
+     *  Represents the lattice where the problem is defined.
+     */
+    Lattice<Descriptor, float_type> lattice;
     
-    // Since cs^2 is constant and it's a problem property it's more efficent to store it as a constexpr inside the Solver
-    // to make the compiler able to inster its value at compile time inside the code preventing the read of the same value
-    // from Lattice at each iteration
+    /*  Since cs^2 is constant and it's a problem property it's more efficient to store
+     *  it as a constexpr inside the Solver to make the compiler able to insert its value
+     *  at compile time inside the code, preventing the read of the same value from
+     *  Lattice at each iteration.
+    */
     constexpr float_type inv_cs2 = 3.0; // 1.0 / (1.0 / 3.0)
     constexpr float_type inv_2cs2 = 1.5; // 2.0 * 1.0 / (1.0 / 3.0)
     constexpr float_type inv_2cs4 = 4.5; // 2.0 * 1.0 / (1.0 / 3.0)^2
 
+    /*
+     *  Lambda function used to compute the scalar product between two arrays,
+     *  it returns the value as a float_type.
+     */
     constexpr auto scalar_prod = [](auto &a, auto &b) {
         float_type res = 0.0;
         for (int i = 0; i < Descriptor::d; ++i) {
@@ -42,26 +41,52 @@ private:
         return res;
     }
 
-    // helper function to compute equilibrium in a single direction
-    inline float_type compute_eq(const std::array<int, Descriptor::d> &c_i, const float_type w_i, const float_type rho, const std::array<float_type, Descriptor::d> &u){
-        return w_i * rho * (1.0 + inv_cs2 * scalar_prod(c_i, u) + inv_2cs4 * scalar_prod(c_i, u) * scalar_prod(c_i, u) - inv_2cs2 * scalar_prod(u, u));
+    /*
+     *  Helper function to compute equilibrium in a single direction.
+     */
+    inline float_type compute_eq(
+            const std::array<int,
+            Descriptor::d> &c_i,
+            const float_type w_i,
+            const float_type rho,
+            const std::array<float_type,
+            Descriptor::d> &u) {
+        return w_i * rho * (
+                1.0 + inv_cs2 * scalar_prod(c_i, u) + 
+                inv_2cs4 * scalar_prod(c_i, u) * scalar_prod(c_i, u) - 
+                inv_2cs2 * scalar_prod(u, u)
+                );
     }; 
     
-    // helper function to compute collision in a single direction
-    inline float_type compute_star(const float_type f_i, const float_type feq_i, const float_type inv_tau_star){
+    /*
+     *  Helper function to compute collision in a single direction.
+     */
+    inline float_type compute_star(
+            const float_type f_i,
+            const float_type feq_i,
+            const float_type inv_tau_star) {
         return f_i * (1 - inv_tau_star) + feq_i * inv_tau_star;
     };
     
-    // function that computes f_next in a single direction
+    /*
+     *  This function encapsulates the computation of equilibrium (via compute_eq),
+     *  collision (via compute_star), propagation in both inner and boundary cells
+     *  for every direction. The function also computes density and velocity of each
+     *  cell of the lattice.
+     */
     void stream_collide(const int i, const float_type inv_tau_star);
     
 public:
 
-    // constructor
-    Solver(Lattice<Descriptor, float_type> &lattice) : lattice(lattice); 
+    /*
+     *  Constructor takes the lattice as argument.
+     */
+    Solver(Lattice<Descriptor, float_type> &lattice_) : lattice(lattice_); 
 
-    // wraps the entire simulation over a user-defined number of iterations (n_iterations) where each step reprents a user-defined time interval
-    // of lenght delta_t
+    /*
+     *  Performs the simulation over a user-defined number of iterations (n_iterations)
+     *  where each step reprents a user-defined time interval of lenght delta_t.
+     */
     void solve(const unsigned long n_iterations, const float_type delta_t);
     
 };
--- src/solver.cpp
+++ src/solver.cpp
@@ -1,8 +1,13 @@
 #include "solver.hpp"
 
+/*
+ *  stream_collide implementation, i is the direction in which it computes the step.
+ */
 template <isDescriptor Descriptor, std::floating_point float_type>
 void Solver<Descriptor, float_type>::stream_collide(const int i, const float_type inv_tau_star) {
-
+    /*
+     *  Lambda function to compute the scalar-vector product.
+     */
     constexpr auto prod = [](auto &a, auto b) {
         std::array<float_type, Descriptor::d> res();
         for (int i = 0; i < Descriptor::d; ++i) {
@@ -12,6 +17,9 @@ void Solver<Descriptor, float_type>::stream_collide(const int i, const float_typ
         return res;
     }
 
+    /*
+     *  Lambda function to compute the vector sum.
+     */
     constexpr auto vector_sum = [](auto &a, auto &b) {
         std::array<float_type, Descriptor::d> res();
         for (int i = 0; i < Descriptor::d; ++i) {
@@ -25,8 +33,11 @@ void Solver<Descriptor, float_type>::stream_collide(const int i, const float_typ
     const float_type w_i = Descriptor::w[i];
     const std::vector<float_type> f_current = lattice.f_current[i];
 
-
-
+    /*
+     *  Iterates over the entire lattice computing f_eq, f_star and f_i for the
+     *  given direction i and summing the contribution to the next density and
+     *  velocity of the cells.
+     */
     for (int y = 0; y < lattice.ny; ++y) {
         for (int x = 0; x < lattice.nx; ++x) {
             int index = y * nx + x; // lattice.idx(x, y)
@@ -36,15 +47,18 @@ void Solver<Descriptor, float_type>::stream_collide(const int i, const float_typ
 
             float_type f_eq = compute_eq(c_i, w_i, rho, u);
 
-            /*
-             * Here we may compute sigma (stress tensor)
-             */
+            // Here we may compute sigma (stress tensor)
 
             float_type f_star = compute_star(f_i, f_eq, tau, delta_t);
 
             float_type rho_w = 0.0;
             std::array<float_type, Descriptor::d> u_w();
 
+            /*
+             *  Checks if the current cell is a boundary one, if so applies
+             *  the bounce back method with the boundary parameters rho_w and u_w,
+             *  otherwise compute the propagation with the push method.
+             */
             if (lattice.isAtBound({x, y}, i, rho_w, u_w)) { // adapt it according to Lattice
                 int i_opp = Descriptor::i_opp[i];
                 f_i = f_star - 2.0 * w_i * rho_w * scalar_prod(c_i, u_w) * inv_cs2;
@@ -55,34 +69,51 @@ void Solver<Descriptor, float_type>::stream_collide(const int i, const float_typ
                 lattice.f_next[i][next_index] = f_i;
             }
 
+            /*
+             *  Compute the contribute that f_i gives to rho_next and u_next.
+             */
             float_type &rho_next = lattice.rho_next[index];
             std::array<float_type, Descriptor::d> &u_next = lattice.u_next[index];
-            if (i == 0){
+            if (i == 0) {
                 rho_next = f_i;
                 u_next = prod(c_i, f_i);
-            }
-            else if (i == Descriptor::q - 1){
+            } else {
                 rho_next += f_i;
                 u_next = vector_sum(u_next, prod(c_i, f_i));
-                u_next = prod(u_next, 1 / rho_next);
-
+                if (i == Descriptor::q - 1) {
+                    u_next = prod(u_next, 1.0 / rho_next);
+                }
             }
-            else {
-                rho_next += f_i;
-                u_next = vector_sum(u_next, prod(c_i, f_i));
-            }
-            
         }
     }
 }
 
+/*
+ *  solve implementation, n_iterations is the length of the simulation
+ *  and delta_t is the time interval length of each step.
+ */
 template <isDescriptor Descriptor, std::floating_point float_type>
-void Solver<Descriptor, float_type>::solve(const unsigned long n_iterations, const float_type delta_t) {
-    const float_type inv_tau_star = 1.0 / ((lattice.nu * delta_t * inv_cs2) / (lattice.delta_x * lattice.delta_x) + 0.5);
+void Solver<Descriptor, float_type>::solve(
+        const unsigned long n_iterations,
+        const float_type delta_t) {
+   
+    /*
+     *  Since tau and delta_t are always used as a ratio between them
+     *  and it is constant over the entire simulation, we compute it
+     *  with respect of tau's relation with problem's parameters.
+     */
+    const float_type inv_tau_star = 1.0 / (
+                (lattice.nu * delta_t * inv_cs2) / 
+                (lattice.delta_x * lattice.delta_x) + 
+                0.5
+            );
 
     // write output
     for (unsigned long n_iter = 0; n_iter < n_iterations; ++n_iter) {
-        // This loop iterates over each possible direction, defined by the LatticeDescriptor, computing f_next
+        /*
+         *  This loop iterates over each possible direction, defined by the LatticeDescriptor,
+         *  computing f_next by calling stream_collide.
+         */
         for (int i = 0; i < Descriptor::q; ++i) {
             stream_collide(i, inv_tau_star);
         }
