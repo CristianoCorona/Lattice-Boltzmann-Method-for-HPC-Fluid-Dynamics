09e29a6ec93bfae2d58405f17b8ce73cdfd2e3cc
Chris <cristianocorona2424@gmail.com>
2025-12-14 11:28:08 +0000

    cluster_main file added and new target LBM_cluster created
    CMake file has been modified to allow a distinct target for cluster
    simulations.

---
+++ src/cluster_main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <array>
+#include <LBM/LBM.hpp>
+
+using float_type = double;
+int main(int argc, char *argv[]) {
+    std::array<int, D2Q9<float_type>::d> lattice_dim = {257, 257};
+    std::array<float_type, D2Q9<float_type>::d> lid_velocity = {0.1, 0.0};
+    float_type Re = 1000.0;
+    float_type L = static_cast<float_type>(lattice_dim[0] - 1);
+    float_type nu = lid_velocity[0] * L / Re;
+
+    Lattice<D2Q9<float_type>, float_type> lattice(
+            lattice_dim,
+            lid_velocity,
+            nu,
+            Direction<D2Q9<float_type>::d>::TOP,
+            "lbm_cluster_output.vtk");
+    
+    Solver<D2Q9<float_type>, float_type> solver(lattice);
+
+    unsigned long n_iter = 200000;
+    float_type delta_t = 1.0;
+    int output_interval = 5000;
+    solver.solve(n_iter, delta_t, output_interval);
+
+    return 0;
+}
