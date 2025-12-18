c7a6d4e3190862b24545ce4782169d5f04f17b18
Federico <federicopizzolato10@gmail.com>
2025-12-18 11:04:53 +0100

    added working parameters

--- src/main3D.cpp
+++ src/main3D.cpp
@@ -4,19 +4,19 @@
 
 int main(int argc, char *argv[]) {
     std::array<int, D3Q19<float>::d> lattice_dim = {19, 19, 19};
-    std::array<float, D3Q19<float>::d> lid_velocity = {1.0f, 0.0f, 0.0f};
+    std::array<float, D3Q19<float>::d> lid_velocity = {0.1f, 0.0f, 0.0f};
     float nu = 1.0f;
 
     Lattice<D3Q19<float>, float> lattice(
             lattice_dim,
             lid_velocity,
             nu,
-            Direction<D3Q19<float>::d>::BOTTOM);
+            Direction<D3Q19<float>::d>::TOP);
     
     Solver<D3Q19<float>, float> solver(lattice);
 
-    unsigned long n_iter = 10000;
-    float delta_t = 1.0f;
+    unsigned long n_iter = 1000;
+    float delta_t = 0.1f;
     solver.solve(n_iter, delta_t);
 
     return 0;
